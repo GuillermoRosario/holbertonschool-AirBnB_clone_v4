57d8028a83aabc619af34c9f5b45a523648fd3be
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-10-04 11:46:53 -0400

    py file fix

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -7,7 +7,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-from uuid import uuid4
+import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -19,13 +19,12 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/4-hbnb/', strict_slashes=False)
+@app.route('/4-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
-    cache_id = uuid4()
 
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
@@ -39,9 +38,9 @@ def hbnb():
     return render_template('4-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id=cache_id)
+                           places=places, cache_id=str(uuid.uuid4()))
 
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
